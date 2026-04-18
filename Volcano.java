ad7a06845edd7fd1bc8ef0f41ee3306c109485ec
elndower <51922927+EngenhariaAc@users.noreply.github.com>
2026-04-18 19:40:02 +0700

    Create Volcano.java

---
+++ Volcano.java
@@ -0,0 +1,11 @@
+public class Volcano {
+    private String name;
+
+    public Volcano(String name) {
+        this.name = name;
+    }
+
+    public void erupt() {
+        System.out.println(name + " erupts explosively, producing rhyolite!");
+    }
+}
